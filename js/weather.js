d0f33accccbf659a272b05f3d92bcd621461b980
Anwar alzazali <158316492+anwar-alzazali@users.noreply.github.com>
2026-09-14 21:43:52 +0700

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,3 @@
+/* [WEATHER.JS] - fetchWeather ada di main.js, file ini untuk modularitas */
+// fetchWeather sudah ada di main.js, wrapper saja
+if(window.fetchWeather) window.fetchWeather = window.fetchWeather;
