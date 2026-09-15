ab7af90d0faf3c333d9558a8bc589a0cca35512e
Anwar alzazali <158316492+anwar-alzazali@users.noreply.github.com>
2026-09-15 13:14:11 +0700

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,9 +1,5 @@
 
-const CACHE = 'tempera-driver-v1';
-const ASSETS = ['/login.html','/manifest.json','/logo-tempera-classic.png','/icon-192.png','/icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE='tempera-driver-v2';
+const ASSETS=['/login.html','/manifest.json','/logo-tempera-classic.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
